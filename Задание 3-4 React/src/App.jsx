396dee2e659c46b6933b1c3c1ce2f3d49557f0a4
Mikael Gevorgyan <92267859+wqross@users.noreply.github.com>
2022-11-05 16:34:47 +0300

    Delete Задание 3-4 React directory

--- Задание 3-4 React/src/App.jsx
+++
@@ -1,8 +0,0 @@
-import { Layout } from "./components/Layout/Layout"
-import { HomePage } from "./pages/HomePage/HomePage"
-
-export const App = () => {
-    return <Layout>
-        <HomePage/>
-    </Layout>
-}
--- Задание 3-4 React/src/components/Main/Main.jsx
+++
@@ -1,7 +0,0 @@
-export const Main = (props) => {
-    return <main className='main'>
-        <div className='container'>
-            <div className="main__inner">{ props.children }</div>
-        </div>
-    </main>
-}
